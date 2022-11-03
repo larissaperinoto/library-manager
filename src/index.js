147f9cfb11e06fb41e501539dcdf53f1e32f8200
Larissa Perinoto <perinotolarissa@gmail.com>
2022-11-03 07:30:18 -0300

    Resolução dos exercícios.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,60 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
 
-const PORT = '3001';
-
+const PORT = '3009';
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
+
+const booksFile = path.resolve(__dirname, './books.json');
+
+// Exercício 5
+app.get('/books/search', async (req, res) => {
+  const { author } = req.query;
+  const booksJson = await fs.readFile(booksFile, 'utf-8');
+  const books = JSON.parse(booksJson);
+  const search = books.filter((book) => book.author.includes(author));
+  res.status(200).json(search);
+});
+
+// Exercício 1
+app.get('/books', async (_req, res) => {
+  const books = await fs.readFile(booksFile, 'utf-8');
+  const response = books ? JSON.parse(books) : [];
+  res.status(200).json(response);
+});
+
+// Exercício 2
+app.post('/books', async (req, res) => {
+  const books = await fs.readFile(booksFile, 'utf-8');
+  const id = books ? JSON.parse(books).length + 1 : 0;
+  const newBook = { ...req.body, id }
+  const newBooksFile = [...JSON.parse(books), newBook]
+  await fs.writeFile(booksFile, JSON.stringify(newBooksFile));
+  res.status(201).json(newBook);
+});
+
+// Exercício 3
+app.put('/books/:id', async (req, res) => {
+  const { id } = req.params;
+  const booksJson = await fs.readFile(booksFile, 'utf-8');
+  const books = JSON.parse(booksJson);
+  const bookToUpdate = books.find((book) => book.id === id);
+  const bookUpdated = { id: bookToUpdate.id, ...req.body };
+  const newBooksFile = books.map((book) => book.id === id ? bookUpdated : book);
+  await fs.writeFile(booksFile, JSON.stringify(newBooksFile));
+  res.status(200).json(bookUpdated);
+});
+
+// Exercício 4
+app.delete('/books/:id', async (req, res) => {
+  const { id } = req.params;
+  const booksJson = await fs.readFile(booksFile, 'utf-8');
+  const books = JSON.parse(booksJson);
+  const newBooksFile = books.filter((book) => book.id !== id);
+  await fs.writeFile(booksFile, JSON.stringify(newBooksFile));
+  res.sendStatus(204);
+});
